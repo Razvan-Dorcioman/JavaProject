f967f36d166cd5f041ced3b1b62cae7b92f80c5e
Razvan Dorcioman <34245071+Razvan-Dorcioman@users.noreply.github.com>
2018-05-07 17:38:07 +0100

    Initial commit

---
+++ src/DBUtil.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+    private static final String mySqlUser = "root";
+    private static final String mySqlPwd = "";
+    private static final String mySqlCS =
+            "jdbc:mysql://localhost:3306/javaprojectdb";
+
+    public static Connection getConnection(DBType dbtype) throws SQLException {
+        switch(dbtype){
+            case ORADB:
+                return null;
+            case MYSQLDB:
+                return DriverManager.getConnection(mySqlCS, mySqlUser, mySqlPwd);
+            default:
+                return null;
+        }
+    }
+}
