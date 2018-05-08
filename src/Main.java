26edd71d24757f06f62defe4f8a855de701fa410
Razvan Dorcioman <34245071+Razvan-Dorcioman@users.noreply.github.com>
2018-05-08 20:18:09 +0100

    First part of interface

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,12 @@
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
             Connection con = DBUtil.getConnection(DBType.MYSQLDB);
 
-            //Selectam datele din tabel
-            String sql_select = "select * from email";
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery(sql_select);
-
-            while(rs.next()){
-                System.out.println(rs.getString(1) + " " + rs.getString(2));
-            }
+        Menu f = new Menu("Java Project");
+        f.setVisible(true);
     }
 
 }
